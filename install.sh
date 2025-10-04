73188355eec90c9e427b941f467d7120213e0154
Jonas Schmidt <schmidtjonas662@gmail.com>
2025-10-04 13:19:08 +0200

    fix: ownership of .config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 echo "Installing Aureli..."
 
 mkdir -p ~/.config/aureli
+sudo chown -R $USER:$USER ~/.config/aureli
 sudo cp bin/aureli /usr/bin
 sudo cp bin/equora /usr/bin
 sudo cp share/wayland-sessions/aureli.desktop /usr/share/wayland-sessions
